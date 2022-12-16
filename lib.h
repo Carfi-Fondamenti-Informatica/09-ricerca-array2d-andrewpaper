7c499df39f1bb396b0daad44854d74f2560aadc8
andrewpaper <118001300+andrewpaper@users.noreply.github.com>
2022-12-16 21:29:07 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,7 +1,9 @@
 #ifndef _GITHUB_LIB_H
 #define _GITHUB_LIB_H
+
 void riempi(char [10][20]);
 void riempi2(char[20]);
 bool conf(char[10][20], char [10]);
 void analisi(char a[10][20], char b[20]);
+
 #endif 
