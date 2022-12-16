194bead2e33f5c7b782be9eb98bdfa5abc051ac1
andrewpaper <118001300+andrewpaper@users.noreply.github.com>
2022-12-16 21:37:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include "lib.h"
 
+using namespace std;
+
+int main()
+{
+    
+    char lista[10][20];
+    char nome[20];
+    riempi(lista);
+    riempi2(nome);
+    conf(lista, nome);
+    analisi(lista, nome);
+    
+  return 0;
+}
